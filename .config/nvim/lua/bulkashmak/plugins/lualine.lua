06c6c731307b7a9a0b05892fe1642e4103d8418a
Bulat <mbmhd2015@gmail.com>
2025-07-14 15:44:21 +0500

    Updated Nvim lualine colors

--- .config/nvim/lua/bulkashmak/plugins/lualine.lua
+++ .config/nvim/lua/bulkashmak/plugins/lualine.lua
@@ -6,34 +6,34 @@ return {
         local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
         local colors = {
-            blue = "#65D1FF",
-            green = "#3EFFDC",
-            violet = "#FF61EF",
-            yellow = "#FFDA7B",
-            red = "#FF4A4A",
-            fg = "#c3ccdc",
-            bg = "#112638",
-            inactive_bg = "#2c3043",
+            silver = "#b5b5b5",
+            aqua = "#00ffff",
+            green = "#aaff00",
+            red = "#c8102e",
+            yellow = "#ffd900",
+            fg = "#f3f3f3",
+            bg = "#2e2e2e",
+            inactive_bg = "#1a1a1a",
         }
 
         local my_lualine_theme = {
             normal = {
-                a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+                a = { bg = colors.silver, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
             },
             insert = {
-                a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+                a = { bg = colors.aqua, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
             },
             visual = {
-                a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+                a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
             },
             command = {
-                a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+                a = { bg = colors.green, fg = colors.bg, gui = "bold" },
                 b = { bg = colors.bg, fg = colors.fg },
                 c = { bg = colors.bg, fg = colors.fg },
             },
